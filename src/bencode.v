b01d698913896f34d2fc07d3a239b71649d92bb6
Pastilhas <joao.campos1221@gmail.com>
2023-10-07 17:30:05 +0100

    Add parse list

--- src/bencode.v
+++ src/bencode.v
@@ -1,27 +1,27 @@
 module main
 
-type Token = []Token | int | map[string]Token | string
+type Token = []Token | []u8 | int | map[string]Token
 
 const (
-	colon = `:`.bytes()[0]
-	endmk = `e`.bytes()[0]
-	lstmk = `l`.bytes()[0]
-	mapmk = `d`.bytes()[0]
-	intmk = `i`.bytes()[0]
+	colon_mark = `:`.bytes()[0]
+	end_mark   = `e`.bytes()[0]
+	list_mark  = `l`.bytes()[0]
+	map_mark   = `d`.bytes()[0]
+	int_mark   = `i`.bytes()[0]
 )
 
 pub fn bdecode(bytes []u8) {
 	mut buf := bytes.clone()
 
 	for buf.len > 0 {
-		if buf[0] == mapmk {
+		if buf[0] == map_mark {
 		}
 	}
 }
 
 fn parse_int(bytes []u8) (Token, []u8) {
 	mut i := 1
-	for ; bytes[i] != endmk; i++ {}
+	for ; bytes[i] != end_mark; i++ {}
 	out := bytes[1..i].bytestr().int()
 	res := bytes[i + 1..]
 	return out, res
@@ -29,10 +29,33 @@ fn parse_int(bytes []u8) (Token, []u8) {
 
 fn parse_str(bytes []u8) (Token, []u8) {
 	mut i := 0
-	for ; bytes[i] != colon; i++ {}
+	for ; bytes[i] != colon_mark; i++ {}
 	size := bytes[..i].bytestr().int()
 	i++
-	out := bytes[i..i + size].bytestr()
+	out := bytes[i..i + size]
 	res := bytes[i + size..]
 	return out, res
+}
+
+fn parse_list(bytes []u8) (Token, []u8) {
+	mut out := []Token{}
+
+	mut buf := bytes.clone()[1..]
+	mut tmp_out := Token(0)
+
+	for buf.len > 0 && buf[0] != end_mark {
+		if buf[0] == int_mark {
+			tmp_out, buf = parse_int(buf)
+		} else if buf[0] == map_mark {
+			tmp_out, buf = parse_map(buf)
+		} else if buf[0] == list_mark {
+			tmp_out, buf = parse_list(buf)
+		} else {
+			tmp_out, buf = parse_str(buf)
+		}
+
+		out << tmp_out
+	}
+
+	return out, buf
 }
