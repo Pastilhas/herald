8381c919e15bafc929c09b6066dda6f4794d7491
Pastilhas <joao.campos1221@gmail.com>
2023-10-10 14:35:29 +0100

    Refactor TrackerConn

--- src/tracker.v
+++ src/tracker.v
@@ -4,10 +4,12 @@
 
 module main
 
+import math
 import net
+import time
 import rand
-import encoding.binary
 import encoding.hex
+import encoding.binary
 
 const (
 	connect_request_prefix  = hex.decode('0x0000_0417_2710_1980_0000_0000')!
@@ -18,10 +20,113 @@ struct TrackerConn {
 mut:
 	transaction_id []u8
 	connection_id  []u8
+	udp            net.UdpConn
+	retry          int
 }
 
-fn announce_to_tracker(addr string) ! {
-	mut conn := net.dial_udp(addr)!
+fn TrackerConn.new(addr string) TrackerConn {
+	udp := net.dial_udp(addr) or {
+		println('eh')
+		exit(1)
+	}
+	mut tracker := TrackerConn{[]u8{}, []u8{}, udp, 0}
+	tracker.udp.set_read_timeout(time.Duration(15))
+
+	return tracker
+}
+
+fn (mut conn TrackerConn) update_connection_timeout() {
+	conn.retry += 1
+	duration := time.Duration(15 * math.powi(2, conn.retry))
+	conn.udp.set_read_timeout(duration)
+}
+
+fn (mut conn TrackerConn) reset_connection_timeout() {
+	conn.retry = 0
+	conn.udp.set_read_timeout(time.Duration(15))
+}
+
+fn (mut conn TrackerConn) send_connect() {
+	conn.transaction_id = rand.bytes(4) or { []u8{len: 4} }
+
+	mut packet := connect_request_prefix.clone()
+	packet << conn.transaction_id
+	conn.udp.write(packet) or {
+		println('Error writting')
+		exit(1)
+	}
+}
+
+fn (mut conn TrackerConn) send_announce() {
+	conn.transaction_id = rand.bytes(4) or { []u8{len: 4} }
+
+	mut packet := conn.connection_id.clone()
+	packet << announce_request_action.clone()
+	packet << conn.transaction_id
+	packet << []u8{len: 20} // info_hash
+	packet << []u8{len: 20} // peer_id
+	packet << []u8{len: 8} // downloaded
+	packet << []u8{len: 8} // left
+	packet << []u8{len: 8} // uploaded
+	packet << []u8{len: 4} // event
+	packet << []u8{len: 4} // IP
+	packet << []u8{len: 4} // key
+	packet << []u8{len: 4} // num_want
+	packet << []u8{len: 2} // port
+
+	conn.udp.write(packet) or {
+		println('Error writting')
+		exit(1)
+	}
+}
+
+fn (mut conn TrackerConn) wait_connect() bool {
+	mut buf := []u8{cap: 512}
+	len, _ := conn.udp.read(mut buf) or {
+		println('Error reading')
+		return false
+	}
+
+	if len < 16 {
+		return false
+	}
+
+	action := binary.big_endian_u32(buf[..4])
+	if action != 0 {
+		return false
+	}
+
+	if buf[4..8] != conn.transaction_id {
+		return false
+	}
+
+	conn.connection_id = buf[8..16].clone()
+	return true
+}
+
+fn (mut conn TrackerConn) wait_announce() bool {
+	mut buf := []u8{cap: 512}
+	_, _ := conn.udp.read(mut buf) or {
+		println('Error reading')
+		exit(1)
+	}
+
+	return false
+}
+
+pub fn announce_to_tracker(addr string) {
+	mut tracker := TrackerConn.new(addr)
+
+	for {
+		tracker.send_connect()
+		if tracker.wait_connect() {
+			break
+		} else {
+			tracker.update_connection_timeout()
+		}
+	}
+
+	tracker.reset_connection_timeout()
 
 	/*
 	while not connected
@@ -30,18 +135,6 @@ fn announce_to_tracker(addr string) ! {
 
 	while connected
 		send announce request
-		receive announce response 
+		receive announce response
 	*/
-}
-
-fn send_connect(mut conn net.UdpConn) ! {
-	packet := []u8{len: 16}
-
-	conn.write(packet)!
-}
-
-fn send_announce(mut conn net.UdpConn) ! {
-	packet := []u8{len: 98}
-
-	conn.write(packet)!
 }
