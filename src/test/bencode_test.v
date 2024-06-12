522eb95b6731f3b6ecafdbd89ffe9a8263de893e
Pastilhas <joao.campos1221@gmail.com>
2024-06-12 11:37:04 +0100

    add tests

--- src/test/bencode_test.v
+++ src/test/bencode_test.v
@@ -1,13 +1,19 @@
-import torrent
+import bencode
 
 fn test_int() {
-	b := torrent.decode('i123e'.bytes()) or { torrent.Token(0) }
+	b := bencode.decode('i123e'.bytes()) or { bencode.Token(0) }
 	assert b is int
 	assert b as int == 123
 }
 
 fn test_str() {
-	b := torrent.decode('6:flower'.bytes()) or { torrent.Token(0) }
+	b := bencode.decode('6:flower'.bytes()) or { bencode.Token(0) }
 	assert b is []u8
 	assert (b as []u8).bytestr() == 'flower'
+}
+
+fn test_lst() {
+	a := 'l4:spami42ee'.bytes()
+	b := bencode.decode(a) or { panic(err) }
+	assert b is []bencode.Token
 }
