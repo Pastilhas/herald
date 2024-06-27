dd6961f20692dc9fc5cc8b5d284a5f3abcd45eb4
Pastilhas <joao.campos1221@gmail.com>
2024-06-27 16:09:41 +0100

    add bitfield fn

--- src/network.v
+++ src/network.v
@@ -118,3 +118,20 @@ fn read_message(message []u8) !(u8, []u8) {
 	payload := message[5..]
 	return id, payload
 }
+
+fn set_piece(mut buf []u8, id int) {
+	i := id / 8
+	j := 7 - id % 8
+
+	mut b := buf[i]
+	b = b | (1 << j)
+	buf[i] = b
+}
+
+fn get_piece(buf []u8, id int) bool {
+	i := id / 8
+	j := 7 - id % 8
+
+	b := buf[i]
+	return (b & (1 << j)) != 0
+}
