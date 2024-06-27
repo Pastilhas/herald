0fdf42cac1d6e581abbc119145f1e05b96f5b2cd
Pastilhas <joao.campos1221@gmail.com>
2024-06-27 15:57:23 +0100

    add write/read messages

--- src/network.v
+++ src/network.v
@@ -5,6 +5,16 @@ import net.urllib
 import net
 import encoding.binary
 
+const msg_choke = u8(0)
+const msg_unchoke = u8(1)
+const msg_interested = u8(2)
+const msg_notinterested = u8(3)
+const msg_have = u8(4)
+const msg_bitfield = u8(5)
+const msg_request = u8(6)
+const msg_piece = u8(7)
+const msg_cancel = u8(8)
+
 fn tracker_url(announce string, hash []u8, peer_id []u8, port u16) !string {
 	mut url := urllib.parse(announce)!
 
@@ -79,4 +89,32 @@ fn read_handshake(handshake []u8, hash []u8, peer []u8) ! {
 	if d != peer {
 		return error('Invalid peer id')
 	}
+}
+
+fn write_message(id u8, payload []u8) []u8 {
+	mut buf := []u8{len: 4, cap: payload.len + 5}
+	binary.big_endian_put_u32(mut buf, u32(payload.len + 1))
+	buf << id
+	buf << payload
+	return buf
+}
+
+fn read_message(message []u8) !(u8, []u8) {
+	if message.len < 5 {
+		return error('Invalid message size')
+	}
+
+	len := binary.big_endian_u32(message)
+	id := message[4]
+
+	if len != message.len + 4 {
+		return error('Invalid length param len <> message.len')
+	}
+
+	if len < 1 {
+		return error('Invalid length param len < 1')
+	}
+
+	payload := message[5..]
+	return id, payload
 }
